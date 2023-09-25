66a95de278ad76b3c136eb7ecf24e1da352ca6bb
Khalid <114026496+loodi181@users.noreply.github.com>
2023-09-25 10:09:52 +0200

    speace

--- src/Pages/home/homeScreen/index.jsx
+++ src/Pages/home/homeScreen/index.jsx
@@ -11,7 +11,7 @@ export default function Home() {
             <AboutMe />
             <MyPortfolio />
             <MySkills />
-            <Footer />
+            <Footer /> 
         </>
     )
 }
